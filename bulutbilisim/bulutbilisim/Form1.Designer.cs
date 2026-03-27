ba30611f96029a056b0951beee7314e7f0142c0c
zoy <test@test.com>
2026-03-27 14:57:16 +0300

    form oluşturuldu

---
+++ bulutbilisim/bulutbilisim/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace bulutbilisim;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        label1 = new System.Windows.Forms.Label();
+        SuspendLayout();
+        // 
+        // label1
+        // 
+        label1.Location = new System.Drawing.Point(140, 54);
+        label1.Name = "label1";
+        label1.Size = new System.Drawing.Size(143, 29);
+        label1.TabIndex = 0;
+        label1.Text = "İlk Oluşturulmuş Label";
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(800, 450);
+        Controls.Add(label1);
+        Text = "Form1";
+        ResumeLayout(false);
+    }
+
+    private System.Windows.Forms.Label label1;
+
+    #endregion
+}
